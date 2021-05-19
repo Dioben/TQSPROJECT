0f64c21439fa6894a926e4da4623da16a20ffeb2
buckaroo69 <diogobento@ua.pt>
2021-05-19 23:14:57 +0100

    initial commit

---
+++ mainApp/src/test/java/g302/tqs/ua/mainapp/MainAppApplicationTests.java
@@ -0,0 +1,13 @@
+package g302.tqs.ua.mainapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
