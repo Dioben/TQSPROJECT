61989c33888f8b4c9df4016646484da430a23498
buckaroo69 <diogobento@ua.pt>
2021-06-04 20:02:12 +0100

    2/?

--- mainApp/src/main/java/logisticsmarshall/tqs/ua/controllers/LogisticsWebController.java
+++ mainApp/src/main/java/logisticsmarshall/tqs/ua/controllers/LogisticsWebController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @org.springframework.web.bind.annotation.RestController
-@RequestMapping("/website/")
+@RequestMapping("")
 public class LogisticsWebController {
 
     @GetMapping(path="/register")
@@ -19,9 +19,7 @@ public class LogisticsWebController {
     }
 
     @PostMapping(path="/register",consumes = "application/json")
-    void processRegisterForm(
-            @RequestParam(name="APIKey") String apikey,
-            User user) {
+    void processRegisterForm(User user) {
         //Placeholder
     }
     @PostMapping(path="/login",consumes = "application/json")
--- mainApp/src/test/java/logisticsmarshall/tqs/ua/controllers/LogisticsWebControllerTest.java
+++ mainApp/src/test/java/logisticsmarshall/tqs/ua/controllers/LogisticsWebControllerTest.java
@@ -4,9 +4,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import logisticsmarshall.tqs.ua.services.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import javax.persistence.Column;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(LogisticsWebController.class)
 class LogisticsWebControllerTest {
     @Autowired
     MockMvc mvc;
@@ -16,42 +25,40 @@ class LogisticsWebControllerTest {
 
     ObjectMapper jsonParser = new ObjectMapper();
 
-
     @Test
-    void whenRegisterAsDriver_EmptyParameters_returnError() throws Exception {
-
+    void whenRegisterAsDriverEmptyParametersReturnError() throws Exception {
+        mvc.perform(post("/register").contentType(MediaType.APPLICATION_JSON).content("{}"))
+                .andExpect(status().is(400));
 
 
     }
 
     @Test
-    void whenRegisterAsDriver_InvalidParameters_returnError() throws Exception {
-        //TODO
+    void whenRegisterAsDriverBadEmailReturnError() throws Exception {
+        mvc.perform(post("/register").contentType(MediaType.APPLICATION_JSON)
+                .content("{" +"\"role\": \"DRIVER\","+
+                            "\"name\": \"testcompanyname\","+
+                            "\"email\": \"istonaotemarrobatextopontoextensao\","+
+                            "\"password\": \"fakesafepassword\""+
+
+
+                        "}"))
+                .andExpect(status().is(400));
     }
 
     @Test
-    void whenRegisterAsDriver_CorrectParameters_returnAPIKey() throws Exception {
-        //TODO
+    void whenRegisterAsDriverCorrectParametersReturnOk() throws Exception {
+        String email = "goodemail@ua.pt";
+        mvc.perform(post("/register").contentType(MediaType.APPLICATION_JSON)
+                .content("{" +"\"role\": \"DRIVER\","+
+                        "\"name\": \"testcompanyname\","+
+                        "\"email\": \""+email+"\","+
+                        "\"password\": \"fakesafepassword\""+
+
+
+                        "}"))
+                .andExpect(status().is(200)).andExpect(content().string(email));
     }
 
-    @Test
-    void whenRegisterAsCompany_EmptyParameters_returnError() throws Exception {
-        //TODO
-    }
-
-    @Test
-    void whenRegisterAsCompany_CorrectParameters_returnAPIKey() throws Exception {
-        //TODO
-    }
-
-    @Test
-    void whenRegisterAsAdmin_EmptyParameters_returnError() throws Exception {
-        //TODO
-    }
-
-    @Test
-    void whenRegisterAsAdmin_CorrectParameters_return200Ok() throws Exception {
-        //TODO
-    }
 
 }
