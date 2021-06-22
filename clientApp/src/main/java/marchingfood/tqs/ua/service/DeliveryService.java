c4891c0654c01bd201f443cf81cfd47cb155e4dd
buckaroo69 <diogobento@ua.pt>
2021-06-22 17:04:52 +0100

    missing ;

--- clientApp/src/main/java/marchingfood/tqs/ua/service/DeliveryService.java
+++ clientApp/src/main/java/marchingfood/tqs/ua/service/DeliveryService.java
@@ -64,7 +64,7 @@ public class DeliveryService {
     public List<ProviderDelivery> getClientDeliveriesFromLogistics(Client client){
         ArrayList<ProviderDelivery> providerDeliveries = new ArrayList<>();
         for(Delivery delivery: client.getOrderEntity()){
-            ProviderDelivery providerDelivery = this.getDeliveryFromLogistics(delivery.getId())
+            ProviderDelivery providerDelivery = this.getDeliveryFromLogistics(delivery.getId());
             providerDeliveries.add(providerDelivery);
             if (providerDelivery.getStage().equals("DELIVERED")){
                 delivery.setDelivered(true);
