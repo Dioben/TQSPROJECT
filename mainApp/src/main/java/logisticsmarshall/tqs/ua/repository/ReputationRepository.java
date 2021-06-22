6ede154628ee4641aa9d586ba85855cf73082957
p-laranjinha <plcasimiro@ua.pt>
2021-06-22 02:19:57 +0100

    Add user's name and email to the admin dashboard

--- mainApp/src/main/java/logisticsmarshall/tqs/ua/repository/ReputationRepository.java
+++ mainApp/src/main/java/logisticsmarshall/tqs/ua/repository/ReputationRepository.java
@@ -12,8 +12,8 @@ import java.util.List;
 
 @Repository
 public interface ReputationRepository extends JpaRepository<Reputation, Long> {
-    @Query("SELECT new logisticsmarshall.tqs.ua.model.DriverAdminView(x.driver.id,x.driver.vehicle,x.driver.phoneNo,avg(x.rating),count(x.rating)) from Reputation x " +
-            "group by x.driver.id,x.driver.phoneNo,x.driver.vehicle" +
+    @Query("SELECT new logisticsmarshall.tqs.ua.model.DriverAdminView(x.driver.id,x.driver.user.name,x.driver.user.email,x.driver.vehicle,x.driver.phoneNo,avg(x.rating),count(x.rating)) from Reputation x " +
+            "group by x.driver.id,x.driver.user.name,x.driver.user.email,x.driver.phoneNo,x.driver.vehicle" +
             " having avg(x.rating)<= :ratingMax and count(x.rating) >= :reviewMin")
     List<DriverAdminView> findAllByRatingMaximum(@Param("ratingMax") double ratingMax,@Param("reviewMin") Long minReviews);
   
