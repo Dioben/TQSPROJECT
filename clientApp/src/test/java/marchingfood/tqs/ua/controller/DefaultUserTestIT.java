0641d657ff2191d4daafa59a937c9f79a64f640d
p-laranjinha <plcasimiro@ua.pt>
2021-06-23 02:14:38 +0100

    Added fully working IT tests

--- clientApp/src/test/java/marchingfood/tqs/ua/controller/DefaultUserTestIT.java
+++ clientApp/src/test/java/marchingfood/tqs/ua/controller/DefaultUserTestIT.java
@@ -1,68 +1,106 @@
 package marchingfood.tqs.ua.controller;
 
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 import io.github.bonigarcia.seljup.SeleniumJupiter;
-import marchingfood.tqs.ua.model.Client;
-import marchingfood.tqs.ua.model.Delivery;
-import marchingfood.tqs.ua.service.DeliveryService;
-import marchingfood.tqs.ua.service.UserServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+import java.util.concurrent.TimeUnit;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ExtendWith(SeleniumJupiter.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class DefaultUserTestIT {
 
-    @Autowired
-    UserServiceImpl userService;
+    @LocalServerPort
+    int serverPort;
 
-    @Autowired
-    DeliveryService deliveryService;
+    String url;
 
     @BeforeEach
     public void setUp() {
-        Client user = userService.getUserByName("user1");
-
-        Delivery delivery = new Delivery();
-        delivery.setDelivered(false);
-        delivery.setAddress("doesn't matter");
-        delivery.setClient(user);
-        delivery.setPaid(false);
-
-        deliveryService.saveDelivery(delivery);
+        url = String.format("http://localhost:%d/", serverPort);
     }
 
     @Test
-    void defaultUserTest(ChromeDriver driver) {//needs to assert something
-        driver.get("http://backend:8000/");
+    void registerUser(ChromeDriver driver){
+        String clientName = "client";
+        String clientEmail = "client@email.com";
+        String clientHome = "home";
+        String clientPass = "randompass";
+
+        driver.get(url);
+        driver.findElement(By.cssSelector("div > .nav-item > .nav-link")).click();
+        driver.findElement(By.linkText("Register here")).click();
+        driver.findElement(By.id("name_com")).sendKeys(clientName);
+        driver.findElement(By.id("email_com")).sendKeys(clientEmail);
+        driver.findElement(By.id("address_com")).sendKeys(clientHome);
+        driver.findElement(By.id("password")).sendKeys(clientPass);
+        driver.findElement(By.cssSelector(".btn")).click();;
+        assertDoesNotThrow(() -> driver.findElement(By.xpath("//h3[contains(.,'Login')]")));
+        driver.findElement(By.linkText("Register here")).click();
+        driver.findElement(By.id("name_com")).sendKeys(clientName);
+        driver.findElement(By.id("email_com")).sendKeys(clientEmail);
+        driver.findElement(By.id("address_com")).sendKeys(clientHome);
+        driver.findElement(By.id("password")).sendKeys(clientPass);
+        driver.findElement(By.cssSelector(".btn")).click();
+        assertDoesNotThrow(() -> driver.findElement(By.xpath("//div[contains(.,'There was an unexpected error (type=Bad Request, status=400).')]")));
+        driver.get(url+"login");
+        driver.findElement(By.id("username")).sendKeys(clientName);
+        driver.findElement(By.id("password")).sendKeys(clientPass);
+        driver.findElement(By.id("login-submit")).click();
+        assertDoesNotThrow(() -> driver.findElement(By.xpath("//a[contains(text(),'Log Out')]")));
+        driver.findElement(By.xpath("//a[contains(text(),'Log Out')]")).click();
+    }
+
+    @Test
+    void defaultUserTest(ChromeDriver driver) throws InterruptedException {
+        driver.get(url);
         driver.manage().window().setSize(new Dimension(1000, 700));
         driver.findElement(By.cssSelector("div > .nav-item > .nav-link")).click();
-//        driver.findElement(By.linkText("Register here")).click();
-//        driver.findElement(By.id("name_com")).sendKeys("client");
-//        driver.findElement(By.id("email_com")).sendKeys("client@email.com");
-//        driver.findElement(By.id("address_com")).sendKeys("home");
-//        driver.findElement(By.id("password")).sendKeys("randompass");
-//        driver.findElement(By.cssSelector(".btn")).click();
         driver.findElement(By.id("username")).sendKeys("user1");
         driver.findElement(By.id("password")).sendKeys("12345");
         driver.findElement(By.id("login-submit")).click();
         driver.findElement(By.cssSelector(".nav-item:nth-child(2) > .nav-link")).click();
-        driver.findElement(By.id("6")).click();
-//        driver.findElement(By.id("#\\31 > .fas")).click();
-/*        driver.findElement(By.cssSelector("div:nth-child(3) > .nav-item > .nav-link")).click();
+        TimeUnit.MILLISECONDS.sleep(4000); // Page needs to load for some buttons to work and alerts to show
+        driver.findElement(By.id("1")).click();
+        assertThat(driver.switchTo().alert().getText(), is("Item added to cart"));
+        driver.switchTo().alert().accept();
+        driver.findElement(By.id("2")).click();
+        assertThat(driver.switchTo().alert().getText(), is("Item added to cart"));
+        driver.switchTo().alert().accept();
+        driver.findElement(By.id("1")).click();
+        assertThat(driver.switchTo().alert().getText(), is("Item added to cart"));
+        driver.switchTo().alert().accept();
+        driver.findElement(By.cssSelector("div:nth-child(3) > .nav-item > .nav-link")).click();
+        TimeUnit.MILLISECONDS.sleep(2000); // Page needs to load for some buttons to work and alerts to show
         driver.findElement(By.xpath("(//button[@type=\'button\'])[2]")).click();
+        assertThat(driver.switchTo().alert().getText(), is("Item removed from cart"));
+        driver.switchTo().alert().accept();
+        TimeUnit.MILLISECONDS.sleep(2000); // Page needs to load for some buttons to work and alerts to show
         driver.findElement(By.cssSelector(".btn-primary")).click();
+        assertThat(driver.switchTo().alert().getText(), is("Delivery Ordered Successfully"));
+        driver.switchTo().alert().accept();
+        TimeUnit.MILLISECONDS.sleep(1000); // Page needs to load for some buttons to work and alerts to show
         driver.findElement(By.cssSelector("div:nth-child(4) .nav-link")).click();
-        driver.findElement(By.id("button17")).click();
-        driver.findElement(By.id("rating")).sendKeys("1");
-        driver.findElement(By.id("description")).sendKeys("Took way too long.");
+        driver.findElement(By.xpath("//td[5]/button")).click();
+        driver.findElement(By.id("rating")).sendKeys("5");
+        driver.findElement(By.id("description")).sendKeys("Good");
         driver.findElement(By.id("submit")).click();
-        driver.findElement(By.cssSelector("div:nth-child(5) .nav-link")).click();*/
+        TimeUnit.MILLISECONDS.sleep(1000); // Page needs to load for some buttons to work and alerts to show
+        assertThat(driver.findElement(By.cssSelector("h2:nth-child(3)")).getText(), is("Your Review has been posted successfully"));
+        driver.findElement(By.xpath("//a[contains(text(),'Log Out')]")).click();
     }
 
 }
